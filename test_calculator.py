5725ade233942abc336e434e4b485168e5c23ad7
joaomendoncagit <joao-mendonca-2077@outlook.com>
2026-04-27 15:00:55 +0100

    Case 3 remove zero division handling

--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,7 @@
 from calculator import add, divide
-import pytest
-
 
 def test_add():
     assert add(2, 3) == 5
 
-
 def test_divide():
     assert divide(6, 2) == 3
-
-
-def test_divide_by_zero():
-    with pytest.raises(ValueError):
-        divide(6, 0)
