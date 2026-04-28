da63f24006fddd3b0146e02d691c715b97be5759
joaomendoncagit <joao-mendonca-2077@outlook.com>
2026-04-28 10:03:45 +0100

    Ensure latest test coverage is included

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,12 @@
 from calculator import add, divide
+import pytest
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_divide():
     assert divide(6, 2) == 3
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(6, 0)
